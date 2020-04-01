9e8d95e76d55f26c75ba4e04ba6f8afc16862215
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-04-01 10:51:47 +0200

    Fix output format

--- sbin/oss_system_status.sh
+++ sbin/oss_system_status.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-echo "{"
-echo "\"runningKernel\":\"$( uname -r )\","
-echo "\"installedKernel\":\"$( rpm -q kernel-default --qf "%{VERSION}-%{RELEASE}" )\","
-echo "\"uptime\":\"$( gawk '{ printf("%d T %d Std",$1/86400,$1%86400/3600) }'  /proc/uptime )\","
-echo "\"version\":\"$( rpm -q --qf "%{VERSION}-%{RELEASE}" oss-base )\","
-d=$( rpm -qa --qf "%{INSTALLTIME}\n" | sort -n | tail  -n 1 )
-echo "\"lastUpdate\":\"$( date -d "@$d" +%Y-%m-%d-%H-%M )\","
+echo -n "{"
+echo -n "\"runningKernel\":\"$( uname -r )\","
+echo -n "\"installedKernel\":\"$( rpm -q kernel-default --qf "%{VERSION}-%{RELEASE}\n" | tail -n1" )\","
+echo -n "\"uptime\":\"$( gawk '{ printf("%d T %d Std",$1/86400,$1%86400/3600) }'  /proc/uptime )\","
+echo -n "\"version\":\"$( rpm -q --qf "%{VERSION}-%{RELEASE}" oss-base )\","
+echo -n "\"lastUpdate\":\"$( rpm -qa --qf "%{INSTALLTIME}\n" | sort -n | tail  -n 1 )\","
 echo -n '"rootUsage":"'
 	df -h --output=avail,pcent,ipcent /     | gawk '{ if ( NR==2 ) { print $1 " " $2 " " $3 "\"" ","} }'
-echo -n '"srvUsage":"'
+isSrv=$( grep " /srv " /proc/mounts  )
+if [ "${isSrv}" ]; then
+   echo -n '"srvUsage":"'
 	df -h --output=avail,pcent,ipcent /srv  | gawk '{ if ( NR==2 ) { print $1 " " $2 " " $3 "\"" ","} }'
-echo -n '"varUsage":"'
+fi
+isVar=$( grep " /var " /proc/mounts  )
+if [ "${isVar}" ]; then
+   echo -n '"varUsage":"'
 	df -h --output=avail,pcent,ipcent /var  | gawk '{ if ( NR==2 ) { print $1 " " $2 " " $3 "\"" ","} }'
-echo -n '"homeUsage":"'
+fi
+isHome=$( grep " /home " /proc/mounts  )
+if [ "${isHome}" ]; then
+    echo -n '"homeUsage":"'
 	df -h --output=avail,pcent,ipcent /home | gawk '{ if ( NR==2 ) { print $1 " " $2 " " $3 "\"" ","} }'
+fi
 echo -n '"availableUpdates":"'
 	zypper lu | gawk '{ if( $1 == "v" ) { printf("%s ", $5) } }'
-	echo '",'
+	echo -n '"'
 echo "}"
 
 
