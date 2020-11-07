f76ce1b7d151faf7413c730467fd97418454950f
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-07 11:50:41 +0100

    Fix typo

--- sbin/crx_system_status.sh
+++ sbin/crx_system_status.sh
@@ -5,7 +5,7 @@ echo -n "\"created\":\"$(date +%s)000\","
 echo -n "\"runningKernel\":\"$( uname -r )\","
 echo -n "\"installedKernel\":\"$( rpm -q kernel-default --qf "%{VERSION}-%{RELEASE}\n" | tail -n1 )\","
 echo -n "\"uptime\":\"$( gawk '{ printf("%d T %d Std",$1/86400,$1%86400/3600) }'  /proc/uptime )\","
-echo -n "\"version\":\"$( rpm -q --qf "%{VERSION}-%{RELEASE}" oss-base )\","
+echo -n "\"version\":\"$( rpm -q --qf "%{VERSION}-%{RELEASE}" cranix-base )\","
 d=$( rpm -qa --qf "%{INSTALLTIME}\n" | sort -n | tail  -n 1 )
 echo -n "\"lastUpdate\":\"$((d*1000))\","
 echo -n '"rootUsage":"'
